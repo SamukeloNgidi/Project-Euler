e22fc906241a5b923878f53caa13ddbb080a475e
Sam <35949504+SamkeloNgidi@users.noreply.github.com>
2019-06-04 20:19:17 +0200

    Update Problem 1 - Java.java

--- Problem 1 - Java.java
+++ Problem 1 - Java.java
@@ -2,7 +2,7 @@ public class Class1 {
     public static void main(String [] args){
         int sum = 0;
 
-        for (int i= 1; i<10; i++){
+        for (int i= 1; i<10; i++){ //1000
            if(i%5==0 || i%3==0){
                System.out.println(i);
               sum+=i;
